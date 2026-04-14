7a226980066b544e5474f26eea772f4d7e10797e
Meurtn <s1167384@student.windesheim.nl>
2026-04-14 19:49:04 +0200

    Links werkend gemaakt

--- includes/footer.php
+++ includes/footer.php
@@ -4,13 +4,12 @@
         <p>&copy; <?php echo date("Y"); ?> Maarten Drost. Alle rechten voorbehouden.</p>
 
         <div class="footer-links">
-            <a href="https://github.com/jouwgebruikersnaam" target="_blank">GitHub</a>
-            <a href="https://linkedin.com/in/jouwprofiel" target="_blank">LinkedIn</a>
+            <a href="https://github.com/DrostMaarten" target="_blank">GitHub</a>
+            <a href="https://linkedin.com/in/maarten-drost-9215901b8" target="_blank">LinkedIn</a>
             <a href="/contact.php">Contact</a>
         </div>
 
     </div>
 </footer>
-
 </body>
 </html>
